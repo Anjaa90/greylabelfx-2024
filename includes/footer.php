b86d8d1b9432940fbc3518a0ef53dd1d8f631fd3
Avinashkdm10 <avinash.prixim@gmail.com>
2025-01-28 15:32:25 +0530

    changes

--- includes/footer.php
+++ includes/footer.php
@@ -139,8 +139,8 @@
     <div class="footer-style-3">
         <div class="copyright-area copyright-style-one variation-two">
             <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-lg-4 col-md-7 col-sm-12 col-12">
+                <div class="row align-items-center ">
+                    <div class="col-lg-5 col-md-7 col-sm-12 col-12">
                         <div class="copyright-left">
                             <ul class="ft-menu link-hover">
                                 <li><a href="privacy-policy.php">Privacy Policy</a></li>
@@ -149,7 +149,7 @@
                             </ul>
                         </div>
                     </div>
-                    <div class="col-lg-4 col-md-5 mt_sm--20">
+                    <div class="col-lg-6 col-md-5 mt_sm--20">
                         <div class="copyright-center text-center">
                             <ul class="social-icon social-default color-lessdark justify-content-center">
                                 <li data-sal="slide-up" data-sal-duration="400" data-sal-delay="200">
@@ -194,7 +194,7 @@
                             </ul>
                         </div>
                     </div>
-                    <div class="col-lg-4 col-md-12 col-sm-12 col-12 mt_md--20 mt_sm--20">
+                    <div class="col-lg-1 col-md-12 col-sm-12 col-12 mt_md--20 mt_sm--20">
                         <div class="copyright-right text-center text-lg-right">
                         </div>
                     </div>
